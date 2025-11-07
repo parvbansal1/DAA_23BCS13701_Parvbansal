b400faf65463bb3be3646cb40ad8073617a19750
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2025-11-07 18:15:31 +0530

    Implement KMP pattern search algorithm

--- Experiment-9/search_pattern.java
+++ Experiment-9/search_pattern.java
@@ -1,1 +1,54 @@
+class Solution {
+    ArrayList<Integer> search(String pat, String txt) {
+           ArrayList<Integer> res = new ArrayList<>();
+        int n = txt.length();
+        int m = pat.length();
 
+        int[] lps = computeLPS(pat);
+
+        int i = 0; 
+        int j = 0; 
+
+        while (i < n) {
+            if (txt.charAt(i) == pat.charAt(j)) {
+                i++;
+                j++;
+            }
+
+            if (j == m) {
+                res.add(i - j);
+                j = lps[j - 1];
+            } else if (i < n && txt.charAt(i) != pat.charAt(j)) {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+
+        return res;
+    }
+
+    int[] computeLPS(String pat) {
+        int m = pat.length();
+        int[] lps = new int[m];
+        int len = 0;
+        int i = 1;
+
+        while (i < m) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
